c748cd99056f0ba900398b34e92bbb41fff16f47
munkg92 <Nurperi-Mavlianbekkyzy.com>
2021-08-27 13:01:07 +0600

    commiting all stuff

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,46 +1,48 @@
-import React from 'react';
+import React from 'react'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams,
-    useRouteMatch
-  } from "react-router-dom";
-export function Topics () {
-    let { path, url } = useRouteMatch();
-    return (
-      <Router>
-          <div>
-        <h2>Topics</h2>
-        <ul>
-          <li>
-            <Link to={`${url}/rendering`}>Rendering with React</Link>
-          </li>
-          <li>
-            <Link to={`${url}/components`}>Components</Link>
-          </li>
-          <li>
-            <Link to={`${url}/props-v-state`}>Props v. State</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path={path}>
-            <h3>Please select a topic.</h3>
-          </Route>
-          <Route path={`${path}/:topicId`}>
-            <Topic />
-          </Route>
-        </Switch>
-      </div>
-      </Router>
-    );
-  }
-  function Topic() {
-    let { topicId } = useParams();
-    return (
-      <div>
-        <h3>{topicId}</h3>
-      </div>
-    );
-  }
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useParams,
+	useRouteMatch,
+} from 'react-router-dom'
+export function Topics() {
+	let { path, url } = useRouteMatch()
+	return (
+		<Router>
+			<div>
+				<h2>Topics</h2>
+				<ul>
+					<li>
+						<Link to={`${url}/rendering`}>
+							Rendering with React
+						</Link>
+					</li>
+					<li>
+						<Link to={`${url}/components`}>Components</Link>
+					</li>
+					<li>
+						<Link to={`${url}/props-v-state`}>Props vs. State</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route exact path={path}>
+						<h3>Please select a topic.</h3>
+					</Route>
+					<Route path={`${path}/:topicId`}>
+						<Topic />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	)
+}
+function Topic() {
+	let { topicId } = useParams()
+	return (
+		<div>
+			<h3>{topicId}</h3>
+		</div>
+	)
+}
